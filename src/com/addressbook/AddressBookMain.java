57457d11875a391976f24e34af5830521d463407
NAVEEDKHAN88 <md.naveedpathan88@gmail.com>
2023-01-12 02:43:32 +0530

    Welcome to addressbook message

---
+++ src/com/addressbook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println(" ------------- WelCome To AddressBook -------------------");
+    }
+}
+
